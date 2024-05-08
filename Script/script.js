4018b9d6cf7635d59fe7c9891fb3ac47e90158ea
Amol Bhosale <amolbhosale2022@gmail.com>
2024-05-08 16:51:16 +0530

    explore-concept-done

--- Script/script.js
+++ Script/script.js
@@ -1,9 +1,13 @@
-// const copy_btn = document.getElementById("copy-btn");
-// copy_btn.addEventListener("click", () => {
-//   var copyText = document.getElementById("copy-text");
-//   copyText.select();
-//   document.execCommand("copy");
-// });
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list-links");
+menuList.classList.add("d-none");
+menuIcon.addEventListener("click", () => {
+  // alert("button clicked");
+  menuIcon.classList.toggle("fa-x");
+  menuList.classList.toggle("d-none");   
+});
+
+
 
 const prev_btn = document.getElementById("prev-btn");
 prev_btn.addEventListener("click", () => {
@@ -27,3 +31,25 @@ copy_btn.addEventListener("click", function copyText() {
       console.error("Could not copy text: ", err);
     });
 });
+
+const copy_btn1 = document.getElementById("copy-btn1");
+copy_btn1.addEventListener("click", function copyText() {
+  var text = document.getElementById("textToCopy1").innerText;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      // alert("Text copied to clipboard");
+      copy_btn1.textContent = "Copied";
+      setTimeout(() => {
+        copy_btn1.textContent = "Copy code";
+      }, 2000);
+      console.error("Text Copied");
+    })
+    .catch((err) => {
+      console.error("Could not copy text: ", err);
+    });
+});
+
+
+
+
