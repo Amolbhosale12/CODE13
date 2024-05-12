d01fcc72e51ea1abeda6f83c0cdb86d7d7096c0a
Amol Bhosale <amolbhosale2022@gmail.com>
2024-05-12 05:51:03 +0530

    all concept of c language

--- Script/script.js
+++ Script/script.js
@@ -7,16 +7,38 @@ menuIcon.addEventListener("click", () => {
   menuList.classList.toggle("d-none");
 });
 
-const lightdark_btn=document.getElementById("checkbox");
-lightdark_btn.addEventListener("click",()=>{
-  const mainBody=document.body;
+const lightdark_btn = document.getElementById("checkbox");
+lightdark_btn.addEventListener("click", () => {
+  const mainBody = document.body;
   mainBody.classList.toggle("dark");
 });
 
 const prev_btn = document.getElementById("prev-btn");
 prev_btn.addEventListener("click", () => {
   window.history.back();
-}); 
+});
+
+
+// copy code
+const copyButtons = document.querySelectorAll(".copy-button");
+copyButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const codeBlocks = document.querySelectorAll("div.code-des");
+    const code = codeBlocks[index].innerText;
+    button.textContent = "copied";
+    setTimeout(() => {
+      button.textContent = "Copy code";
+    }, 4000);
+    navigator.clipboard
+      .writeText(code)
+      .then(() => {
+        // alert("Code copied successfully!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy code: ", err);
+      });
+  });
+});
 
 const copy_btn = document.getElementById("copy-btn");
 copy_btn.addEventListener("click", function copyText() {
@@ -53,4 +75,3 @@ copy_btn1.addEventListener("click", function copyText() {
       console.error("Could not copy text: ", err);
     });
 });
-
